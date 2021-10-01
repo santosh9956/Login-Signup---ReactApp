4a0d9a3344cc282b3604aaf21dbbd3dbea2da6fc
Navgurukul2k19 <santhosh@appscrip.co>
2021-10-01 12:50:35 +0530

    done all changes with the firebase

--- src/Components/Parent.js
+++ src/Components/Parent.js
@@ -8,9 +8,9 @@ function Parent() {
   return (
     <>
     <div  className=' d-flex justify-content-center align-items-center pt-5'>
-      <div class="card col-12 col-sm-6" >
+      <div className="card col-12 col-sm-6" >
         <img className='h-50'  src="https://thumbs.dreamstime.com/z/beautiful-small-statue-lord-ganesha-clay-lamp-rose-petals-against-red-golden-background-hinduism-religion-concept-172858013.jpg" class="card-img-top" alt="Image_things" />
-        <div class="card-body d-flex flex-column align-items-center">
+        <div className="card-body d-flex flex-column align-items-center">
               <button className="signup_button " >
                 <Link className='text_decoration signup_Link' to='/signup/firstname'> Create an account </Link>
               </button>
